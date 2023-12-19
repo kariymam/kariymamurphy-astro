8c97fa57c7441c596ed05aff4906bca699573574
kariymam <59487728+kariymam@users.noreply.github.com>
2023-12-18 21:04:58 -0500

    latestPosts styles and animation

---
+++ src/_includes/_partials/latestPosts/latestPosts.js
@@ -0,0 +1,46 @@
+
+/*
+1. hear the hover of a postlistItem
+	item.addEventListener('hover', event => {
+
+	})
+function
+. get postlistItem instance (done)
+. get the height of the closest h3 in postlistItem instance = headerHeight (done)
+. retrieve the headerHeight add 16px = moveY (done)
+. call moveY value
+. move thumbImg using top with moveY value
+. when mouse leaves postlistItem
+. move thumbImg back to top 0
+
+gsap.fromTo(".thumbImg", { top: 0 }, { top: `${moveY}`, duration: 1 });
+
+*/
+/* THIS IS WORKING TO GET  VALUES!!!!
+
+let tl = gsap.timeline();
+const postlistItem = document.querySelectorAll(".postlist-item");
+
+postlistItem.forEach(post => {
+  post.addEventListener("mouseover", (e) => {
+
+		let q = gsap.utils.selector(e.target.closest("article"));
+		const headerHeight = e.target.closest("article").querySelector("h3").offsetHeight;
+		const moveY = headerHeight + 16;
+
+		tl.to(q(".thumbImg"), {
+			duration: 0.2,
+			ease: "power1.out",
+			top: `${moveY}px`
+		});
+
+	})
+	post.addEventListener("mouseleave", () => {
+		tl.to((".thumbImg"), {
+			duration: 0.3,
+			ease: "power1.in",
+			top: 0
+		});
+	})
+});
+*/
---
+++ src/blog/Second Post.md
@@ -0,0 +1,12 @@
+---
+title: This is an even longer title of a post look how long this is wow so many words so many possibilities
+description: I was hired to brand and design social media flyer for We Own This! This is an example of a really long description, what’s up what’s happening lorem ipsum dolor sit.
+date: 2023-10-03
+tags:
+  - joe pera
+  - gus
+thumb: https://res.cloudinary.com/dvf7zwben/image/upload/v1681832476/000541330005_iipoqv.jpg
+thumbAlt: Blurry photo of someone going down an elevator
+---
+
+Some text.
