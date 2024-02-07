088d76e71fab3cfd737560129c8624288da12ab5
kariymam <59487728+kariymam@users.noreply.github.com>
2024-02-06 22:05:39 -0500

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
 			'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
 		},
 		fontSize: {
-      sm: '0.8rem',
+			xs: '0.75rem',
+      sm: '0.875rem',
       base: '1rem',
-      xl: '1.5rem',
-      '2xl': '3.516rem',
-      '3xl': '4.766rem',
-      '4xl': '6rem',
-      '5xl': '7.25rem',
+      xl: '1.125rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
     },
 		extend: {
 			colors: {
