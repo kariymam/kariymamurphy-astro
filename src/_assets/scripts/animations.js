253f08ec87a697a43c747fd0b5cb2da63054d7eb
kariymam <59487728+kariymam@users.noreply.github.com>
2024-02-11 04:30:37 -0500

    scroll update

--- src/_assets/scripts/animations.js
+++ src/_assets/scripts/animations.js
@@ -25,10 +25,7 @@ const animations = () => {
 			// console.log("Window height:", window.innerHeight);
 			// console.log("Document height:", html.clientHeight);
 			return (
-					rect.top >= 0 &&
-					rect.left >= 0 &&
-					rect.bottom <= (window.innerHeight || html.clientHeight) &&
-					rect.right <= (window.innerWidth || html.clientWidth)
+				rect.left >= 0 && rect.right <= (window.innerWidth || html.clientWidth)
 			);
 		}
 
@@ -42,143 +39,110 @@ const animations = () => {
 			}
 		}
 
-		const scrollContainer = document.querySelector('.overflow-scroll');
-		const latestPostsList = document.querySelector('#latestPostsList');
-		const postlistHeavyItems = latestPostsList.querySelectorAll('.postlist-heavyItem');
+		const latestPostsList = document.querySelector("#latestPostsList");
+		const postlistHeavyItems = latestPostsList.querySelectorAll(
+			".postlist-heavyItem"
+		);
 
 		postlistHeavyItems.forEach((el) => {
-			scrollContainer.addEventListener(
+			latestPostsList.addEventListener(
 				"scroll",
 				function () {
 					runOnScroll(el);
 				},
 				{ passive: true }
 			);
-			window.addEventListener("load",runOnScroll(el));
+			window.addEventListener("load", runOnScroll(el));
 		});
 	}
 
 	function lastestPostsSlider() {
-    // the current .postlist-heavyItem
-    const current = document.querySelector('.current');
+		const latestPostsList = document.querySelector("#latestPostsList");
+		// the current .postlist-heavyItem
+		const current = latestPostsList.querySelector(".current");
 
-    // the scroll forward button in latestPostsList
-    const slideBtn = latestPostsList
-        ? latestPostsList.closest(".flex").querySelector('button')
-        : null;
+		// the scroll forward button in latestPostsList
+		const slideBtn = latestPostsList
+			? latestPostsList.closest(".flex").querySelector("button")
+			: null;
 
-    // the scroll back button
-    const slideBackBtn = document.getElementById('slideBack');
+		// the scroll back button
+		const slideBackBtn = document.getElementById("slideBack");
 
-    const slideForward = () => {
-        const nextSibling = current.nextElementSibling;
-        if (nextSibling !== null) {
-            console.log("There is a next sibling"); // slide forward
-            nextSibling.scrollIntoView({ behavior: 'smooth' });
-        } else {
-            console.log("There is no next sibling");
-        }
-    }
+		if (!slideBtn) {
+			return; // Exit the function if no matching ancestor is found
+		}
 
-    const slideBack = () => {
-        const previousSibling = current.previousElementSibling;
-        if (previousSibling !== null) {
-            console.log("There is a previous sibling"); // slide back
-            slideBackBtn.classList.add("lg:flex");
-            previousSibling.scrollIntoView({ behavior: 'smooth' });
-        } else {
-            console.log("There is no previous sibling");
-        }
-    }
+		const childCount = () => {
+			let fchild = latestPostsList.firstElementChild.children.length;
+			return (fchild -= 1);
+		};
 
-    if (slideBtn) {
-        slideBtn.addEventListener('click', slideForward);
-    }
-    if (slideBackBtn) {
-        slideBackBtn.addEventListener('click', slideBack);
-    }
-}
+		let tr = 0;
+		let clickCounter = 0;
+		const maxClicks = childCount();
 
+		const slideAnimation = (direction) => {
+			const width = current.clientWidth;
+			tr += direction === "forward" ? -width : width;
+			let slideTL = gsap.timeline({ paused: true });
+			latestPostsList.style.scrollSnapType = "none";
 
+			slideTL.to(".scroll-container", {
+				duration: 0.3,
+				ease: "power2.inOut",
+				snap: 0.5,
+				x: `${tr}`,
+				onComplete: () => {
+					latestPostsList.style.scrollSnapType = "x mandatory";
+					// Check if translation has reached the initial state
+					if (tr === 0 || clickCounter === 0) {
+						slideBackBtn.classList.remove("lg:flex");
+					} else {
+						slideBackBtn.classList.add("lg:flex");
+					}
+				},
+			});
 
-	// function lastestPostsSlider() {
-	// 	const latestPostsList = document.querySelector("#latestPostsList");
-	// 	const slideBtn = latestPostsList
-	// 		? latestPostsList.closest(".flex").querySelector("button")
-	// 		: null;
-	// 	const slideBackBtn = document.getElementById("slideBack");
+			return slideTL;
+		};
 
-	// 	if (!slideBtn) {
-	// 		return; // Exit the function if no matching ancestor is found
-	// 	}
+		const slideForward = () => {
+			const nextSibling = current.nextElementSibling;
+			if (nextSibling !== null) {
+				console.log("There is a next sibling"); // slide forward
+				const tl = slideAnimation("forward");
+				tl.play();
+				clickCounter++;
+				checkClickCount();
+			} else {
+				console.log("There is no next sibling");
+			}
+		};
 
-	// 	const childCount = () => {
-	// 		let fchild = latestPostsList.firstElementChild.children.length;
-	// 		return (fchild -= 1);
-	// 	};
+		const slideBack = () => {
+				const tl = slideAnimation("backward");
+				tl.play();
+				clickCounter--;
+				checkClickCount();
+		};
 
-	// 	let tr = 0;
-	// 	let clickCounter = 0;
-	// 	const maxClicks = childCount();
+		const checkClickCount = () => {
+			if (clickCounter >= maxClicks) {
+				slideBtn.classList.remove("lg:flex");
+			} else {
+				slideBtn.classList.add("lg:flex");
+			}
+			return console.log(clickCounter);
+		};
 
-	// 	const slideAnimation = (direction) => {
-	// 		const slidePost = latestPostsList.querySelector(".postlist-heavyItem");
-	// 		const width = slidePost.clientWidth;
-	// 		tr += direction === "forward" ? -width : width;
-
-	// 		let slideTL = gsap.timeline({ paused: true });
-
-	// 		slideTL.to(latestPostsList, {
-	// 			duration: 0.3,
-	// 			ease: "power2.inOut",
-	// 			x: `${tr}`,
-	// 			onComplete: () => {
-	// 				// Check if translation has reached the initial state
-	// 				if (tr === 0 || clickCounter === 0) {
-	// 					slideBackBtn.classList.remove("lg:flex");
-	// 				} else {
-	// 					slideBackBtn.classList.add("lg:flex");
-	// 				}
-	// 			},
-	// 		});
-
-	// 		return slideTL;
-	// 	};
-
-	// 	const slideForward = () => {
-	// 		const tl = slideAnimation("forward");
-	// 		tl.play();
-	// 		clickCounter++;
-	// 		checkClickCount();
-	// 	};
-
-	// 	const slideBack = () => {
-	// 		const tl = slideAnimation("backward");
-	// 		tl.play();
-	// 		clickCounter--;
-	// 		checkClickCount();
-	// 	};
-
-	// 	const checkClickCount = () => {
-	// 		if (clickCounter >= maxClicks) {
-	// 			slideBtn.classList.remove("lg:flex");
-	// 		} else {
-	// 			slideBtn.classList.add("lg:flex");
-	// 		}
-	// 		return console.log(clickCounter);
-	// 	};
-
-	// 	slideBtn.addEventListener("click", slideForward);
-	// 	slideBackBtn.addEventListener("click", slideBack);
-	// 	window.addEventListener("resize", () => {
-	// 		if (window.innerWidth <= 1024) {
-	// 			// Reset the x property to 0 if the window width is less than or equal to 1024px
-	// 			tr = 0;
-	// 			gsap.to(latestPostsList, { x: tr, duration: 0 }); // Use GSAP to set x property instantly
-	// 			clickCounter = 0;
-	// 		}
-	// 	});
-	// }
+		if (slideBtn) {
+			slideBtn.addEventListener("click", slideForward);
+		}
+		if (slideBackBtn) {
+			slideBackBtn.addEventListener("click", slideBack);
+		}
+	}
 
 	function postlistImgs() {
 		const headerHeight = (a, b) => a + b;
