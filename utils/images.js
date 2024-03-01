790cb88d22fbf30f87c6987a45be63c4734b4ec7
kariymam <59487728+kariymam@users.noreply.github.com>
2024-02-29 22:08:28 -0500

    deploy eleventyImages

--- utils/images.js
+++ utils/images.js
@@ -2,7 +2,7 @@ const path = require("path");
 const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
-
+// let's see if this works
 	eleventyConfig.addShortcode("image", async function(src, alt, css) {
 		if(alt === undefined) {
 			// You bet we throw an error on missing alt (alt="" works okay)
