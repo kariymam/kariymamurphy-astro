3ff199a39a12901fd09f62e83dd96d42ea8c62f9
kariymam <59487728+kariymam@users.noreply.github.com>
2025-04-01 14:24:33 -0400

    Add video option to starter template and scheme

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const work = defineCollection({
         deviceFrame: z.boolean(),
         image: z.object({ url: z.string(), alt: z.string() }),
         mobile: z.object({ url: z.string(), alt: z.string() }).optional(),
+        video: z.object({ title: z.string(), videoCode: z.string() }).optional(),
         gallery: z.array(z.object({ image: z.string(), alt: z.string() })).optional()
     })
 })
