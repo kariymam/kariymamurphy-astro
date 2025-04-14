ddb13fca362c16f48ec824017f48d237fc715da0
kariymam <59487728+kariymam@users.noreply.github.com>
2025-04-14 15:04:28 -0400

    Adding booking link

--- src/components/FooterContact.tsx
+++ src/components/FooterContact.tsx
@@ -9,7 +9,7 @@ const FooterContact = () => {
             id="footer-contact"
             className="max-w-full w-full flex flex-col gap-4 md:w-xl p-8 bg-slate-950 text-4xl text-white z-[1]"
         >
-            <p>Interested in working together? <a onFocus={scrollUp} className="underline" href={`mailto:${`kariymam@proton.me`}`}>Email me</a> or book a call.</p>
+            <p>Interested in working together? <a onFocus={scrollUp} className="underline" href={`mailto:${`kariymam@proton.me`}`}>Email me</a> or <a id="contact_booking_link" href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2ri1OdgqIsLq9eoY8PNMxmehuEd-JNLGV1VcsqEa714GL0x20mtOudTHVbd9DZTFnuMj2F_6MX">book a call.</a></p>
         </div>
     )
 }
